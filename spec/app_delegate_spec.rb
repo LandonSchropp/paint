04dc0641dd309db3dd3bd6b1b779f11fe9a2fe7c
Landon Schropp <schroppl@gmail.com>
2014-05-04 13:53:53 -0700

    Added specs for the app delegate

---
+++ spec/app_delegate_spec.rb
@@ -0,0 +1,21 @@
+describe AppDelegate do
+
+  describe "#application:didFinishLaunchingWithOptions:" do
+
+    before do
+      @application = UIApplication.sharedApplication
+    end
+
+    it "creates the window" do
+      @application.windows.size.should == 1
+    end
+
+    it "makes the window key" do
+      @application.windows.first.isKeyWindow.should.be.true
+    end
+
+    it "sets the root view controller" do
+      @application.windows.first.rootViewController.should.be.instance_of UIViewController
+    end
+  end
+end
