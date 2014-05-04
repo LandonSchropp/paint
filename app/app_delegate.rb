2594109e3d5cb289b3c3d5177a419b4084846aca
Landon Schropp <schroppl@gmail.com>
2014-05-04 13:51:47 -0700

    Added window creation to the app delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,10 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+    @window.rootViewController = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+
     true
   end
 end
